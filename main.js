598b7d34c40ac6075058be2b56edec55482e5cfb
Jason M <jason.moposita1201@gmail.com>
2025-04-22 13:00:46 -0400

    Add logic to determine computer choice

--- main.js
+++ main.js
@@ -1,1 +1,16 @@
-console.log('Hello World');
+function getComputerChoice() {
+  const compChoice = Math.ceil(Math.random() * 3)
+  
+  switch (compChoice) {
+    case 1:
+      return 'rock'
+      break;
+    case 2:
+      return 'paper'
+      break;
+    case 3: 
+      return 'scissors'
+      break;
+  }
+}
+ 
